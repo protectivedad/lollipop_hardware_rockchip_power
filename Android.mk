28696ce5e5166dc44b720933baca32fbb422e859
Cody Xie <xkd@rock-chips.com>
2014-12-13 14:56:29 +0800

    Android.mk : compatible with multilib

--- Android.mk
+++ Android.mk
@@ -19,7 +19,14 @@ LOCAL_PATH := $(call my-dir)
 # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 include $(CLEAR_VARS)
 
+ifneq (1,$(strip $(shell expr $(PLATFORM_VERSION) \>= 5.0)))
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+else
+ifneq ($(strip $(TARGET_2ND_ARCH)), )
+LOCAL_MULTILIB := both
+endif
+LOCAL_MODULE_RELATIVE_PATH := hw
+endif
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_SRC_FILES := power.c
 LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
